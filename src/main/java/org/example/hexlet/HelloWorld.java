4ae58338ec2e311d65269762b168990b2d127036
Arlan <Arlan110022@Gmail.com>
2024-08-17 15:24:15 +0500

    update

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -5,11 +5,31 @@ import io.javalin.Javalin;
 public class HelloWorld {
     public static void main(String[] args) {
         // Создаем приложение
-        var app = Javalin.create(config -> {
-            config.bundledPlugins.enableDevLogging();
-        });
-        // Описываем, что загрузится по адресу /
+        var app = Javalin.create(config -> config.bundledPlugins.enableDevLogging());
+
+        // Обработчик для маршрута "/"
         app.get("/", ctx -> ctx.result("Hello World"));
-        app.start(7070); // Стартуем веб-сервер
+
+        // Обработчик для маршрута "/hello"
+        app.get("/hello", ctx -> {
+            String name = ctx.queryParam("name"); // Получаем параметр name или используем значение "World" по умолчанию
+            ctx.result("Hello, " + name + "!"); // Формируем и отправляем приветствие
+        });
+
+        // Обработчик для маршрута /courses/{id}
+        app.get("/courses/{id}", ctx -> ctx.result("Course ID: " + ctx.pathParam("id")));
+
+        // Обработчик для маршрута /users/{id}
+        app.get("/users/{id}", ctx -> ctx.result("User ID: " + ctx.pathParam("id")));
+
+        // Обработчик для маршрута /users/{id}/post/{postId}
+        app.get("/users/{id}/post/{postId}", ctx -> {
+            var userId = ctx.pathParam("id");
+            var postId = ctx.pathParam("postId");
+            ctx.result("User ID: " + userId + " Post ID: " + postId);
+        });
+
+        // Стартуем веб-сервер
+        app.start(7070);
     }
 }
